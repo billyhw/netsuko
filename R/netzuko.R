4a68629f197462a50a97e560400013e97f3b488b
Billy Chang <billyheungwing@gmail.com>
2021-12-29 19:21:47 +0800

    allowing multiple outputs in least_square

--- R/netzuko.R
+++ R/netzuko.R
@@ -95,7 +95,7 @@ cross_entropy = function(p, y) -mean(rowSums(y*log(p)))
 #' @return The least square error (halved)
 #' @note For Internal Use. Note the least square error is halved
 #' to simplify the gradients
-least_square = function(p, y) mean((y - p)^2)/2
+least_square = function(p, y) mean(rowSums((y - p)^2))/2
 
 #' Compute the gradient of the weight for a given layer
 #'
